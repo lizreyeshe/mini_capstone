2fb6ea4a9b5ed98ca5f2566fa27d647136cce347
liz <elizabeth.reyes@ifrsf.org>
2018-05-27 14:15:50 -0700

    created a route to display all_products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+
+namespace :api do 
+
+  get "/all_products" => "products#all_products_action"
+
+end 
+
 end
